5992b4d6d377322b7fa4ac29ded42b178c73a8d6
Syrenate <luke.jennings1@outlook.com>
2026-07-28 16:11:36 +0100

    Account Implementation
    Began implementation of account features like sign up, sign in and sign out, with session detection. Still need to implement password reset.
    
    Development is slow right now while I still get used to working with http requesting with supabase. All new stuff, but its all interesting.

--- src/utils/supabase_connect.py
+++ src/utils/supabase_connect.py
@@ -1,15 +1,14 @@
-import os, supabase
+import os
 from flask import Flask, request, url_for, redirect
-from supabase import create_client, Client, SupabaseAuthClient
-from supabase import SupabaseAuthClient as auth
+from supabase import create_client, Client
 from dotenv import load_dotenv
 
 load_dotenv("src/vars.env")
 app = Flask(__name__)
 
 supabase_client: Client = create_client(
-    os.environ.get("SUPABASE_URL"),
-    os.environ.get("SUPABASE_PUBLISHABLE_KEY")
+    supabase_url=os.environ.get("SUPABASE_URL"),
+    supabase_key=os.environ.get("SUPABASE_PUBLISHABLE_KEY")
 )
 
 
@@ -24,27 +23,23 @@ def add_reservation(user_id: int, instruments: list[int], date: str, start_time:
         }
     ]).execute()
 
-def click_button():
-    print("clicked")
-
-
 def DisplayDataTable(name: str):
     response = supabase_client.table(name).select("*").execute()
-    data = response.data
+
+    camel_name = ' '.join([word[0].upper() + word[1:] for word in name.split(" ")])
 
     html = '<style> table, th, td { border: 1px solid black; } </style>'
-    html += f'<h1>{name}</h1><table><tr>'
+    html += f'<h1>{camel_name}</h1><table><tr>'
 
-    print(data)
     try:
-        for column in data[0].keys():
+        for column in response.data[0].keys():
             html += f'<th>{column}</th>'
         html += '</tr>'
     except IndexError as e:
-        print("Database is empty!")
-        return None
+        print(f"Database {name} is empty!")
+        return ""
 
-    for row in data:
+    for row in response.data:
         html += '<tr>'
         for column in row.keys():
             html += f'<td>{row[column]}</td>'
@@ -53,35 +48,64 @@ def DisplayDataTable(name: str):
 
     return html
 
+@app.route('/', methods = ["GET", "POST"])
+def root():
+    session = supabase_client.auth.get_session()
 
-@app.route('/')
-def index():
-    #html = "<button onclick=click_button()>Click here</button>"
-    html = ""
-    for table in ["instruments", "accounts", "reservations"]:
-        html += DisplayDataTable(table) + "<br>"
-    return html
+    if request.method == "GET":
+        if session == None: html = open("src/pages/root_unlogged.html").read() 
+        else:               html = f"Logged in as: {session.user.user_metadata["full_name"]} " + open("src/pages/root_logged.html").read()
+
+        for table in ["instruments", "reservations"]:
+            html += DisplayDataTable(table) + "<br>"
+        return html
+
+    if request.form.get('signout') != None:
+        response = supabase_client.auth.sign_out()
+        return redirect(url_for("root"), Response="GET")
+    elif request.form.get('signin') != None:
+        return redirect(url_for("signin"), Response="GET")
+    elif request.form.get('signup') != None:
+        return redirect(url_for("signup"), Response="GET")
+
 
 @app.route('/signup', methods = ["GET", "POST"])
 def signup():
     if request.method == "GET":
         return open("src/pages/signup.html").read()
 
-    email = request.form["email"]
-    password = request.form["pass"]
-
-    auth.sign_up(supabase_client, credentials={
-        email,
-        password,
+    if request.form.get('back') == "Back":
+        return redirect(url_for("root"), Response="GET")
+    
+    
+    print(request.form["email"], request.form["pass"])
+    response = supabase_client.auth.sign_up(credentials={
+        "email": request.form["email"],
+        "password": request.form["pass"],
+        "options": {
+            "data": {
+                "full_name": request.form["name"]
+            }
+        }
     })
 
+    return redirect(url_for("signin"), Response="GET")
 
-    redirect(url_for("login"))
 
+@app.route('/signin', methods = ["GET", "POST"])
+def signin():
+    if request.method == "GET":
+        return open("src/pages/signin.html").read()
 
-@app.route('/login', methods = ["GET", "POST"])
-def login():
-    return open("src/pages/login.html").read()
+    if request.form.get('back') == "Back":
+        return redirect(url_for("root"), Response="GET")
+    
+    response = supabase_client.auth.sign_in_with_password(credentials={
+        "email": request.form["email"],
+        "password": request.form["pass"]
+    })
+
+    return redirect(url_for("root"), Response="GET")
 
     
 
